1cc6fd2f0f357af9f1e637afe34ac5095b282ce6
Pradip Bhusnar <bhusnarpradip@gmail.com>
2023-06-17 10:43:17 +0530

    PSB | added test cases for dashboard

--- src/components/Dashboard/__test__/Dashboard.test.tsx
+++ src/components/Dashboard/__test__/Dashboard.test.tsx
@@ -1,8 +1,16 @@
-import { render, screen } from '@test-utils';
+import { urls } from '@constant/urls';
+import { fireEvent, mockAxios, renderWithRouter, screen, waitFor } from '@test-utils';
+import * as mockData from '@utils/testData/applications.json';
 import { Dashboard } from '../Dashboard';
 
-const renderer = () => render(<Dashboard />);
+const renderer = () => renderWithRouter(<Dashboard />);
 describe('Dashboard', () => {
+    beforeEach(() => {
+        jest.resetModules();
+        jest.resetAllMocks();
+        mockAxios.resetHistory();
+    });
+
     it('should render properly', () => {
         const { container } = renderer();
         expect(container).toMatchSnapshot();
@@ -12,5 +20,52 @@ describe('Dashboard', () => {
         renderer();
         expect(screen.getAllByText('Applications')).toHaveLength(2);
         expect(screen.getByText('Resources')).toBeInTheDocument();
+    });
+
+    it('should show loader', () => {
+        renderer();
+        expect(screen.getAllByText('Applications')).toHaveLength(2);
+        expect(screen.getByTitle('loader')).toBeInTheDocument();
+    });
+
+    it('should fetch all the applications', async () => {
+        mockAxios.onGet(urls.applications).reply(200, mockData);
+        renderer();
+        expect(screen.getAllByText('Applications')).toHaveLength(2);
+        expect(screen.getByTitle('loader')).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.queryByTitle('loader')).not.toBeInTheDocument();
+            expect(screen.getByText('dummyTitle')).toBeInTheDocument();
+        });
+    });
+
+    it('should show no result available message on failure or in case response is empty', async () => {
+        mockAxios.onGet(urls.applications).networkError();
+        renderer();
+        expect(screen.getAllByText('Applications')).toHaveLength(2);
+        expect(screen.getByTitle('loader')).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.queryByTitle('loader')).not.toBeInTheDocument();
+            expect(screen.getByText('No Applications available')).toBeInTheDocument();
+        });
+    });
+
+    it('should go to the resources tab when we click on resources tab', () => {
+        renderer();
+        fireEvent.click(screen.getByText('Resources'));
+        expect(screen.getAllByText('Resources')).toHaveLength(2);
+    });
+
+    it('should fetch all the resources', async () => {
+        mockAxios.onGet(urls.applications).reply(200, mockData);
+        mockAxios.onGet(urls.resources).reply(200, mockData);
+        renderer();
+        fireEvent.click(screen.getByText('Resources'));
+        expect(screen.getAllByText('Resources')).toHaveLength(2);
+        expect(screen.getByTitle('loader')).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.queryByTitle('loader')).not.toBeInTheDocument();
+            expect(screen.getByText('dummyTitle')).toBeInTheDocument();
+        });
     });
 });
